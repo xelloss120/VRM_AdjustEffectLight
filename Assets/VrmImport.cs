573bbcd7e915273afb1dc5f71879c03b1ebdc227
120 <xelloss120@hotmail.com>
2024-08-24 03:12:39 +0900

    読み込み

---
+++ Assets/VrmImport.cs
@@ -0,0 +1,25 @@
+using System.Threading.Tasks;
+using UnityEngine;
+using SFB;
+using VRM;
+
+public class VrmImport : MonoBehaviour
+{
+    public void OnClick()
+    {
+        Async();
+    }
+
+    async void Async()
+    {
+        await Import();
+    }
+
+    async Task Import()
+    {
+        var paths = StandaloneFileBrowser.OpenFilePanel("Open File", "", "vrm", false);
+        var instance = await VrmUtility.LoadAsync(paths[0]);
+        instance.EnableUpdateWhenOffscreen();
+        instance.ShowMeshes();
+    }
+}
